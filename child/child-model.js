38aee919e3c71419f490fd68e66f1d3b5f2ef29b
Jonathan Ho <johnnyho82@gmail.com>
2019-10-14 16:54:02 -0400

    Uncomment troublesome code

--- child/child-model.js
+++ child/child-model.js
@@ -25,19 +25,19 @@ function find() {
 function findById(id) {
   const promises = [
     db('children')
-      // .join('countries', 'countries.id', 'children.country_id')
-      // .join('communities', 'communities.id', 'children.community_id')
-      // .select(
-      //   'children.id as child_id',
-      //   'children.name',
-      //   'children.parent_name',
-      //   'children.contact',
-      //   'children.gender',
-      //   'children.DOB',
-      //   // 'countries.country',
-      //   'communities.community',
-      // )
-      .where({ id })
+      .join('countries', 'countries.id', 'children.country_id')
+      .join('communities', 'communities.id', 'children.community_id')
+      .select(
+        'children.id as child_id',
+        'children.name',
+        'children.parent_name',
+        'children.contact',
+        'children.gender',
+        'children.DOB',
+        'countries.country',
+        'communities.community',
+      )
+      .where({ child_id: id })
       .first(),
     findScreenings(id),
   ];
--- community/community-model.js
+++ community/community-model.js
@@ -20,17 +20,15 @@ function find() {
 }
 
 function findById(id) {
-  return (
-    db('communities')
-      // .join('countries', 'countries.id', 'communities.country_id')
-      // .select(
-      //   'communities.id as community_id',
-      //   'communities.community',
-      //   'countries.country',
-      // )
-      .where({ id })
-      .first()
-  );
+  return db('communities')
+    .join('countries', 'countries.id', 'communities.country_id')
+    .select(
+      'communities.id as community_id',
+      'communities.community',
+      'countries.country',
+    )
+    .where({ community_id: id })
+    .first();
 }
 
 function findChildren(id) {
